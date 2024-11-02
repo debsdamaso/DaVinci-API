d401d529b8c897b6450bcfa010133e47e60e6d15
debsdamaso <deboracert@gmail.com>
2024-11-02 17:49:02 -0300

    adicionando MLNET

---
+++ myMLApp/Program.cs
@@ -0,0 +1,13 @@
+﻿using MyMLApp;
+// Adicione uma frase
+var sampleData = new MLModel.ModelInput()
+{
+    Col0 = "material de baixa qualidade parece barato"
+};
+
+// Carrega o modelo e prevê uma saída de dado de exemplo
+var result = MLModel.Predict(sampleData);
+
+// Se a predicao 1, sentimento "Positivo"; se nao, sentimento "Negativo"
+var sentiment = result.PredictedLabel == 1 ? "Positivo" : "Negativo";
+Console.WriteLine($"Text: {sampleData.Col0}\nSentiment: {sentiment}");
